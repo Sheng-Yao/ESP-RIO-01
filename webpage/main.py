9c0924adc4d0aa0bf83129c6a605fdedf8e4971a
Sheng-Yao <hengshengyao2002@gmail.com>
2025-12-26 02:28:01 +0800

    Implemented new webpage design

--- webpage/main.py
+++ webpage/main.py
@@ -4,3 +4,7 @@ import gzip
 with open("index.html", "rb") as i, gzip.open("../webpage_gzip/index.html.gz", "wb") as o: shutil.copyfileobj(i, o)
 with open("login.html", "rb") as i, gzip.open("../webpage_gzip/login.html.gz", "wb") as o: shutil.copyfileobj(i, o)
 with open("style.css", "rb") as i, gzip.open("../webpage_gzip/style.css.gz", "wb") as o: shutil.copyfileobj(i, o)
+
+with open("index.html", "rb") as i, gzip.open("../../ESP32_Relay/littlefs/index.html.gz", "wb") as o: shutil.copyfileobj(i, o)
+with open("login.html", "rb") as i, gzip.open("../../ESP32_Relay/littlefs/login.html.gz", "wb") as o: shutil.copyfileobj(i, o)
+with open("style.css", "rb") as i, gzip.open("../../ESP32_Relay/littlefs/style.css.gz", "wb") as o: shutil.copyfileobj(i, o)
